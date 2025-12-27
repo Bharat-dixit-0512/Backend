f2f9e28e7cc9769a8556dc6c3f03534352b2738f
Bharat-dixit-0512 <dixitbt251@gmail.com>
2025-12-27 23:34:59 +0530

    cloudinary and some handlers & middlewars multer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from './db/index.js'
+import { app } from "./app.js";
 dotenv.config({
     path:'C:/Users/dixit/Desktop/Backend/.env'
 });
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`🚩 Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log('MongoDB connection Failed ERROR:', error)
+})
 
 
 
